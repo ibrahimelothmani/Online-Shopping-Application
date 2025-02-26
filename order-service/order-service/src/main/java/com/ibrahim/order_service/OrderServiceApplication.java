7c3ea14baeed95809f3ecc4eaca3928b5fb0ab36
Ibrahim <ibrahimelothmani98@gmail.com>
2025-02-26 17:04:11 +0100

    Microservice last-touch

--- order-service/order-service/src/main/java/com/ibrahim/order_service/OrderServiceApplication.java
+++ order-service/order-service/src/main/java/com/ibrahim/order_service/OrderServiceApplication.java
@@ -3,11 +3,9 @@ package com.ibrahim.order_service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @Slf4j
-@EnableFeignClients
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
--- order-service/order-service/src/main/java/com/ibrahim/order_service/service/OrderService.java
+++ order-service/order-service/src/main/java/com/ibrahim/order_service/service/OrderService.java
@@ -3,41 +3,53 @@ package com.ibrahim.order_service.service;
 
 import com.ibrahim.order_service.client.InventoryClient;
 import com.ibrahim.order_service.dto.OrderRequest;
+import com.ibrahim.order_service.event.OrderPlacedEvent;
 import com.ibrahim.order_service.model.Order;
 import com.ibrahim.order_service.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.kafka.core.KafkaTemplate;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
+
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class OrderService {
 
+    private static final Logger log = LoggerFactory.getLogger(OrderService.class);
     private final OrderRepository orderRepository;
     private final InventoryClient inventoryClient;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public void placeOrder(OrderRequest orderRequest) {
-        boolean inStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
-        if (inStock) {
-            var order = mapToOrder(orderRequest);
+
+        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+        if (isProductInStock) {
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price().multiply(BigDecimal.valueOf(orderRequest.quantity())));
+            order.setSkuCode(orderRequest.skuCode());
+            order.setQuantity(orderRequest.quantity());
             orderRepository.save(order);
+            var orderPlacedEvent = new OrderPlacedEvent(order.getOrderNumber(),
+                    orderRequest.userDetails().email(),
+                    orderRequest.userDetails().firstName(),
+                    orderRequest.userDetails().lastName());
+            log.info("Start- Sending OrderPlacedEvent {} to Kafka Topic", orderPlacedEvent);
+            kafkaTemplate.send("order-placed", orderPlacedEvent);
+            log.info("End- Sending OrderPlacedEvent {} to Kafka Topic", orderPlacedEvent);
         } else {
-            throw new RuntimeException("Product with Skucode " + orderRequest.skuCode() + "is not in stock");
+            throw new RuntimeException("Product with SkuCode " + orderRequest.skuCode() + " is not in stock");
         }
     }
 
-    private static Order mapToOrder(OrderRequest orderRequest) {
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
-        order.setQuantity(orderRequest.quantity());
-        order.setSkuCode(orderRequest.skuCode());
-        return order;
-    }
 
     public List<Order> getOrder() {
         return orderRepository.findAll();
@@ -45,6 +57,6 @@ public class OrderService {
 
     public Order getOrderById(Long id) {
         return orderRepository.findById(id)
-                .orElseThrow();
+                .orElseThrow(() -> new RuntimeException("Order with id " + id + " not found"));
     }
 }
